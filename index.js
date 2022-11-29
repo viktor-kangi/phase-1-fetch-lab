f4e4befde277d546cd9c66b556c4c828b2c9637d
victor <victor.buthia@student.moringaschool.com>
2022-11-29 09:34:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+
+}
+
+  function renderBooks(books) {
 }
 
 function renderBooks(books) {
